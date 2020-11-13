4feab68c63d1554ee826cb7ae2fad1b552e68225
ayman <aimans@post.bgu.ac.il>
2020-11-13 15:13:36 +0200

    13/11/20 15:13

--- parser_module.py
+++ parser_module.py
@@ -1,5 +1,6 @@
 import re
 
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from document import Document
@@ -15,20 +16,7 @@ class Parse:
         :return:
         """
         text_tokens = word_tokenize(text)
-        text_tokens_with_hashtag=[]
         i = 0
-        while i != len(text_tokens):
-            if text_tokens[i]=='#':
-
-                listHashTags=re.findall('[A-Z][^A-Z]*',text_tokens[i+1])
-                for w in listHashTags:
-                    text_tokens_with_hashtag.append(w.lower())
-                text_tokens_with_hashtag.append(text_tokens[i]+text_tokens[i+1].lower())
-                i+=2
-            else:
-                text_tokens_with_hashtag.append(text_tokens[i])
-                i+=1
-
         text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
         text_tokens_with_tags = []
         i=0
@@ -39,7 +27,9 @@ class Parse:
             else:
                 text_tokens_with_tags.append(text_tokens_without_stopwords[i])
                 i+=1
-
+        for i in text_tokens_without_stopwords:
+            self.convertURL(i)
+            self.convertNumber()
         return text_tokens_with_tags
 
     def parse_doc(self, doc_as_list):
@@ -71,4 +61,112 @@ class Parse:
                             quote_url, term_dict, doc_length)
         return document
 
+    def bind_tokens_by_caps(self,list_tokens):
+        return_list = []
+        prev_string = ""
+        for token in list_tokens:
+            if token[0].isupper():
+                if prev_string == "":
+                    prev_string = token
+                else:
+                    prev_string += ' ' + token
+            else:
+                if prev_string != '':
+                    return_list.append(prev_string)
+                return_list.append(token)
+                prev_string = ''
+        if prev_string != '':
+            return_list.append(prev_string)
+        return return_list
 
+    def expend_not_shortcut(self,word):
+        return_value = word
+        index_find = word.find("'")
+        if index_find == -1:
+            return return_value
+        index__find = word.find("'t")
+        if index__find != -1:
+            if index_find > 0 and word[index_find-1] == 'n':
+                index_find = index_find - 1
+            return_value = word[0:index_find] + ' not'
+        return return_value
+
+    def convert_text_line_to_tokens(self,line):
+        return nltk.word_tokenize(line)
+
+    def convert_text_to_lines(self,text):
+        return nltk.sent_tokenize(text)
+
+    def find_sub_text_indexes(self,hashtag):
+        word = ''
+        return_list = []
+        for letter in hashtag:
+            if letter.isupper():
+                if not word.isupper() and len(word) >0 :
+                    return_list.append(word)
+                    word = ''
+            elif word.isupper() and len(word) != 1:
+                return_list.append(word)
+                word = ''
+            word += letter
+        if word != '':
+            return_list.append(word)
+        return return_list
+
+    def set_Upper_or_lower(self,word_list):
+        # create dict for lower and upper list
+        lower_set = dict()
+        upper_dic = dict()
+        # run on th Word_list
+        for index in range(len(word_list)):
+            word = word_list[index]  # get the word in index
+            if not word[0].isupper():  # a lower word
+                lower_set[word.lower()] = 0  # add word to lower dic
+                if upper_dic.get(word.upper()) is not None:  # if appear in upper_dic
+                    indexes_list = upper_dic.get(word.upper())  # get list of indexes in list
+                    # run all over list and change to lower
+                    for upper_word_index in indexes_list:
+                        word_list[upper_word_index] = word.lower()
+                    indexes_list = []  # no need to update more
+            elif lower_set.get(word.lower()) is not None:  # Start with capital letter and in lower
+                word_list[index] = word.lower()
+            else:  # only seen as upper
+                list_value = upper_dic.get(word.upper())  # if list already exist
+                # update list if none
+                if list_value is None:
+                    list_value = []
+                list_value.append(index)  # add to list upper word
+                upper_dic[word.upper()] = list_value  # add the list to dic
+                word_list[index] = word.upper() # change to upper word
+        return word_list
+
+    def convertURL(self,URL):
+        _treebank_word_tokenizer = nltk.NLTKWordTokenizer()
+        lst=URL.split('/')
+        y=[token for sent in lst for token in _treebank_word_tokenizer.tokenize(sent)]
+        lstToken=[]
+        for i in y:
+            if i[0].isalpha() or i[0].isdigit():
+                lstToken.append(i)
+        return [w for w in lstToken if w not in self.stop_words]
+        return lstToken
+
+    def convertNumber(self,num,str):
+        if num>=1000 and num<1000000:
+            return num/1000+'K'
+        if num>=1000000 and num<1000000000:
+            return num/1000000+'M'
+        if num>=1000000000:
+            return num/1000000000+'B'
+        if str.lower()=="thousand":
+            return num+'K'
+        if str.lower()=="million":
+            return num+'M'
+        if str.lower()=="billion":
+            return num+'B'
+        if str.lower()=="percent" or str.lower()=="percentage":
+            return num+"%"
+
+if __name__ == '__main__':
+    p=Parse()
+    print(p.stop_words)
--- reader.py
+++ reader.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 
-
 class ReadFile:
     def __init__(self, corpus_path):
         self.corpus_path = corpus_path
