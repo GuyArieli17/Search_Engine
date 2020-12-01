d5045c666dd8d3461eab108eeb0a0640d6634842
aiman-sa <62476837+aiman-sa@users.noreply.github.com>
2020-12-02 00:39:13 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,11 +1,11 @@
 class ConfigClass:
+
     def __init__(self,corpusPath='',output_path='', stemming=False ):
         self.corpusPath = corpusPath
         self.savedFileMainFolder = output_path
         self.saveFilesWithStem = self.savedFileMainFolder + "/WithStem"
         self.saveFilesWithoutStem = self.savedFileMainFolder + "/WithoutStem"
         self.toStem = stemming
-
         print('Project was created successfully..')
 
     def get__corpusPath(self):
