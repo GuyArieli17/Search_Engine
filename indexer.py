6caa25a5cb59f9bbeffeef3eb0238877895bcc17
aiman-sa <62476837+aiman-sa@users.noreply.github.com>
2020-12-02 00:40:17 +0200

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -1,25 +1,31 @@
-from MapReduce import MapReduce
-from nltk.corpus import util
-from configuration import ConfigClass
 from parser_module import Parse
 import psutil
+from MapReduce import MapReduce
 import sys
-
 class Indexer:
-
     def __init__(self, config, all_terms_dict):
+        #laala
         self.inverted_idx = all_terms_dict
-        self.postingDict = {}
         self.fileName = 'InvertedIndex'
         self.config = config
+        #lalala
         # {term: [ordered list where appear : (file_id , lineNumber)]}
         self.thread_pool_size = 5
-        avg_ram = (psutil.virtual_memory().available // 7)//self.thread_pool_size
-        self.avg_length =(avg_ram // sys.getsizeof((int(), str()))) // (8/10)
-        self.map_reduce = MapReduce(self.avg_length,self.thread_pool_size)
+        avg_ram = (psutil.virtual_memory().available //(self.thread_pool_size *2))
+        self.avg_length = (avg_ram // sys.getsizeof((int(), str()))) //2
+        self.map_reduce = MapReduce(self.avg_length,self.thread_pool_size,'MapReduceData/')
+        # self.doc_freq_map_reduce = MapReduce(self.avg_length,self.thread_pool_size,'MapReduceDocData/')
         self.tmp_pos = {}
         self.num_in_pos_tmp = 0
+        self.Entitys = {}
 
+    # def add_doc_to_frquence(self,document_dictionary,current_term):
+    #     term1_key = current_term
+    #     frequence_doc =self.tmp_pos
+    #     term1_value = document_dictionary[term1_key]
+    #     for term2_key, term2_value in document_dictionary.items():
+    #         if (term1_key, term2_key) not in frequence_doc.keys() and (term2_key, term1_key) not in frequence_doc.keys():
+    #             frequence_doc[(term1_key, term2_key)] = [term1_value * term2_value]
 
     def add_new_doc(self, document):
         """
@@ -28,16 +34,13 @@ class Indexer:
         :param document: a document need to be indexed.
         :return: -
         """
-        max_term_list = dict()
         document_dictionary = document.term_doc_dictionary
-        number_of_terms = 0
         for term in document_dictionary.keys():
-            number_of_terms += 1
-            if term in max_term_list.keys():
-                max_term_list[term] += 1
-            else:
-                max_term_list[term] = 1
             try:
+                # frequence_doc = {}  # {(term1,term2):thier_value(f1*f2)}
+                if term[0].isupper() and " " in term:
+                    self.addEntitysToPosting(term,document.tweet_id,document_dictionary[term])
+                    continue
                 if self.num_in_pos_tmp >= self.avg_length:
                     self.map_reduce.write_dict(self.tmp_pos)
                     self.tmp_pos.clear()
@@ -45,10 +48,25 @@ class Indexer:
                 if term.lower() not in self.tmp_pos.keys():
                     self.tmp_pos[term.lower()] = []
                 self.tmp_pos[term.lower()].append((document.tweet_id, document_dictionary[term]))
+                self.tmp_pos[('Document',document.tweet_id)] = document_dictionary
                 self.num_in_pos_tmp += 1
             except:
                 print('problem with the following key {}'.format(term[0]))
-        # max_term = max(max_term_list.keys())
+        max_freq = max([document_dictionary.values()])
+
+    def addEntitysToPosting(self,term,tweet_id,quantity):
+        if term.upper() not in self.Entitys.keys() and term.upper() not in self.tmp_pos.keys():
+            self.Entitys[term.upper()]=(tweet_id,quantity)
+        else:
+            if term.upper() not in self.inverted_idx.keys():
+                self.inverted_idx[term.upper()]=2
+            else:
+                self.inverted_idx[term.upper()]+=1
+            if term.upper() not in self.tmp_pos.keys():
+                self.tmp_pos[term.upper()] = []
+                self.tmp_pos[term.upper()].append(self.Entitys[term.upper()])
+                del self.Entitys[term.upper()]
+            self.tmp_pos[term.upper()].append((tweet_id,quantity))
 
 
 if __name__ == '__main__':
