2ebee289ddf26ce6cb5fd537a332927092c1dfa6
aiman-sa <62476837+aiman-sa@users.noreply.github.com>
2020-12-10 20:30:45 +0200

    Update document.py

--- document.py
+++ document.py
@@ -3,18 +3,6 @@ class Document:
     def __init__(self, tweet_id, tweet_date=None, full_text=None, url=None, indices=None, retweet_text=None, retweet_urls=None, retweet_indices=None,
                  quoted_text=None, quote_urls=None, quoted_indices=None, retweet_quoted_text=None, retweet_quoted_urls=None, retweet_quoted_indice=None,
                  term_doc_dictionary=None):
-        """
-        :param tweet_id: tweet id
-        :param tweet_date: tweet date
-        :param full_text: full text as string from tweet
-        :param url: url
-        :param retweet_text: retweet text
-        :param retweet_url: retweet url
-        :param quote_text: quote text
-        :param quote_url: quote url
-        :param term_doc_dictionary: dictionary of term and documents.
-        :param doc_length: doc length
-        """
         self.tweet_id = tweet_id
         self.tweet_date = tweet_date
         self.full_text = full_text
