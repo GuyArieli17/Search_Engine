c640e4fe9370dd3371093328ad53fd2b876f67fb
aiman-sa <62476837+aiman-sa@users.noreply.github.com>
2020-12-10 20:33:20 +0200

    Update searcher.py

--- searcher.py
+++ searcher.py
@@ -38,30 +38,3 @@ class Searcher:
                 print('term {} not found in posting'.format(term))
         relevant_docs["META-DATA"] = meta_data_dict
         return relevant_docs
-
-
-        # for term in query:
-        #     try: # an example of checks that you have to do
-        #         posting_doc = posting[term] # list of all doc containt term
-        #         for doc_tuple in posting_doc:
-        #             doc = doc_tuple[0]
-        #             if doc not in relevant_docs.keys():
-        #                 relevant_docs[doc] = 1
-        #             else:
-        #                 relevant_docs[doc] += 1
-        #     except:
-        #         print('term {} not found in posting'.format(term))
-        return relevant_docs
-
-        # q : donald trump had corana last week
-        # q-parse:  donald trump corana last week
-        # q-terms: [donald, trump,donald trump , corona, last,week]
-        # covid week impact donald trump  = > [covid,week, impact,donald,trump,donald trump]
-        # doc has all word in englist lung
-      #
-        # tf =  number of term in doc/number of word in doc
-        # idf = log( N(number of documents)/ number of doc with term)
-        # w = tf * idf
-        #
-        #
-        #
